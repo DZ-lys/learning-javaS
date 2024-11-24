fed9b33d68120b95c477bae459095184cb38c128
DZed <idoy0utubebtw@gmail.com>
2024-11-25 01:40:44 +0800

    nothing special

--- Desktop/class/javascript/condition.js
+++ Desktop/class/javascript/condition.js
@@ -27,15 +27,69 @@
  
  
 
-let dorem = false;
-let score = 19;
-if (score > 18){
-    if (dorem) {
-        console.log("pass")
-    }else {
-        console.log("unable to go on")
+// let dorem = false;
+// let score = 19;
+// if (score > 18){
+//     if (dorem) {
+//         console.log("pass")
+//     }else {
+//         console.log("unable to go on")
+//     }
+// }
+// else{
+//     console.log("not enough points")
+// }
+
+// let too = 19283752;
+// let leftover = too % 2;
+// if(leftover == 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+// let too1 = 19283751;
+// if(too1 % 7 == 0){
+//     console.log("can be divided");
+// } else {
+//     console.log("cant be devided");
+// }
+
+// can be devided by 7 and 11
+// can be devided by 7 but cant be devided by 11
+// cant be devided by 7 but can be devided by 11
+// cant be devided by either 7 and 11
+
+// let username = "andy";
+// let password = 1234;
+// if (username == "andy") {
+//     if (password == 1234){
+//         console.log("logging in");
+//     } else {
+//         console.log("incorrect password");
+//     }
+// } else {
+//     if (password == 1234) {
+//         console.log("incorrect username");
+//     } else {
+//         console.log("incorrect username and password");
+//     }
+// }
+
+let budaa1 = 15;
+let budaa2 = 14.9;
+let budaa3 = 14.8;
+
+if (budaa2 > budaa1) {
+    if (budaa2 > budaa3){
+        console.groupCollapsed("budaa2");
+    } else {
+        console.log("budaa3");
     }
-}
-else{
-    console.log("not enough points")
+} else {
+    if (budaa1 > budaa3) {
+        console.log("budaa1");
+    } else {
+        console.log("budaa3");
+    }
 }
